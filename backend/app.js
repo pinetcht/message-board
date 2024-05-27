ad6ffd1917c2e61e3179468d5d3701d901d9adef
Pine <pinetcht@gmail.com>
2024-05-27 17:10:15 +0200

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+// import require("dotenv").config()
+const express = require("express");
+const app = express
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+app.request(express.json());
