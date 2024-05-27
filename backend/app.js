cc67407c1c8e80180fc728003dfd3f6b235bed39
Pine <pinetcht@gmail.com>
2024-05-27 20:18:35 +0200

    Add clear form after submit

--- backend/app.js
+++ backend/app.js
@@ -12,3 +12,46 @@ const { collection, getDocs, updateDoc, doc, addDoc } = require("firebase/firest
 const cors = require("cors");
 app.use(cors());
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+// add post
+app.post("/post", async (req, res) => {
+    try {
+        const username = req.body.username;
+        const message = req.body.message;
+
+        const docRef = await addDoc(collection(db, "post"), {
+            username: username,
+            message: message
+        });
+        res.status(200).json({message: `Successfully added post to firebase with id: ${docRef.id}`});
+    } catch(e) {
+        res.status(400).json({error: `Error adding post with ${e.message} error`})
+    }
+
+})
+
+// add post
+app.get("/post", async (req, res) => {
+    try {
+        let ret = [];
+        const docRef = await getDocs(collection(db, "post"));
+
+        docRef.forEach((doc) => {
+            ret.push({
+                id: doc.id,
+                ...doc.data()
+            })
+        })
+
+
+        res.status(200).json({message: "Successfully fetched post from firebase"});
+    } catch(e) {
+        res.status(400).json({error: `Error fetching post with ${e.message} error`})
+    }
+
+})
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,67 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  
+  const [username, setUsername] = useState("enter username");
+  const [message, setMessage] = useState("type your message");
+  const [allPosts, setAllPosts] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:5000/post");
+    console.log("response", response.data);
+    setAllPosts(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = {
+      username: username,
+      message: message,
+    };
+
+    const response = await axios.post("http://localhost:5000/post", body);
+    console.log(response);
+
+    fetchData();
+
+    setUsername("");
+    setMessage("");
+  };
 
   return (
     <>
-    
+      <h1> Message Board App</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Username:&nbsp;</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          id="form"
+        ></input>
+        <br></br>
+        <br></br>
+        <label id="form">
+          Message: &nbsp;
+        </label>
+        <textarea
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          style={{ height: "100px" }}
+        ></textarea>
+        <br></br>
+        <button type="submit">submit</button>
+      </form>
+
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
